246d31ee04a2b35f716356b24a766498ad3d0d98
alsonsim <alsonsimck@gmail.com>
2026-01-27 23:26:57 +0800

    Level 1. Echo, Improved Deo so that they will echo

--- src/main/java/Deo.java
+++ src/main/java/Deo.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Deo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________";
+
         System.out.println(line);
         System.out.println(" Hello! I'm Deo");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        String input = "";
+        while (!input.equals("bye")) {
+            input = scanner.nextLine();
+            System.out.println(line);
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(" " + input);
+            }
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
