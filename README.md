a5bbdf0b98096ffe2733a6c479a3472e0da7a566
alsonsim <alsonsimck@gmail.com>
2026-02-19 22:53:42 +0800

    Added ascii art

--- README.md
+++ README.md
@@ -16,11 +16,46 @@ Prerequisites: JDK 17, update Intellij to the most recent version.
 1. After that, locate the `src/main/java/Duke.java` file, right-click it, and choose `Run Duke.main()` (if the code editor is showing compile errors, try restarting the IDE). If the setup is correct, you should see something like the below as the output:
    ```
    Hello from
-    ____        _        
-   |  _ \ _   _| | _____ 
+    ____        _
+   |  _ \ _   _| | _____
    | | | | | | | |/ / _ \
    | |_| | |_| |   <  __/
    |____/ \__,_|_|\_\___|
    ```
 
 **Warning:** Keep the `src\main\java` folder as the root folder for Java files (i.e., don't rename those folders or move Java files to another folder outside of this folder path), as this is the default location some tools (e.g., Gradle) expect to find Java files.
+
+                         PROBLEM SOLVING
+                          P R O C E S S
+
+
+            YES   =============================   NO
+     +-----------|| Does the Darn Thing work? ||-----------+
+     |            =============================            |
+     V                                                     V
+
++----------+ +---------+ +---------+
+| Don't | NO | Does | +-------+ YES | Did you |
+| mess | +---| anyone |<------| YOU |<---------| mess |
+| with it! | | | know? | | MORON | | with it |
++----------+ | +---------+ +-------+ +---------+
+| V | YES | NO
+| +------+ +-----------+ |
+| | HIDE | V V
+| | IT | +--------+ +-----------+
+| +------+ | YOU | YES | WILL THEY |
+| | +------->| POOR |<------------| CATCH YOU?|
+| | | |BASTARD!| +-----------+
+| | | |**\_\_\_\_**| | NO
+| | | | |
+| | | V V
+| | | +---------------+ +-----------+
+| | | NO | CAN YOU BLAME | |DESTROY THE|
+| | +------| SOMEONE ELSE? | | EVIDENCE |
+| | +---------------+ +-----------+
+| | | YES |
+| | v |
+| | ============================ |
+| +---->|| N O ||<---------+
++------------>|| P R O B L E M ||
+============================
--- src/main/java/deo/Deo.java
+++ src/main/java/deo/Deo.java
@@ -5,25 +5,35 @@ import logic.DeoLogic;
 public class Deo {
     private static final DeoLogic logic = new DeoLogic();
 
+    private static final String ASCII_ART = "___________   _______________________________________^__\n"
+                                    + " ___   ___ |||  ___   ___   ___    ___ ___  |   __  ,----\\\n"
+                                    + "|   | |   |||| |   | |   | |   |  |   |   | |  |  | |_____\\\n"
+                                    + "|___| |___|||| |___| |___| |___|  | O | O | |  |  |        \\\n"
+                                    + "           |||                    |___|___| |  |__|         )\n"
+                                    + "___________|||______________________________|______________/\n"
+                                    + "           |||     I'd rather be at DisneyWorld...    /--------\n"
+                                    + "-----------'''---------------------------------------'\n";
+
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        System.out.println(ASCII_ART);
         System.out.println(line);
         System.out.println(" Hello! I'm Deo");
         System.out.println(" What can I do for you?");
         System.out.println(line);
 
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String input = scanner.nextLine().trim();
-            System.out.println(line);
-            logic.handleCommand(input);
-            if (logic.isExit()) {
-                System.out.println(" Bye. Hope to see you again soon!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String input = scanner.nextLine().trim();
                 System.out.println(line);
-                break;
+                logic.handleCommand(input);
+                if (logic.isExit()) {
+                    System.out.println(" Bye. Hope to see you again soon!");
+                    System.out.println(line);
+                    break;
+                }
+                System.out.println(line);
             }
-            System.out.println(line);
         }
-        scanner.close();
     }
 }
