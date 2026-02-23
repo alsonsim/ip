05ea661ce2336d19341e5812d0a010bdc8ae0a2c
alsonsim <alsonsimck@gmail.com>
2026-02-23 15:32:14 +0800

    Extract TaskList class

---
+++ src/main/java/deo/logic/TaskList.java
@@ -0,0 +1,41 @@
+package deo.logic;
+
+import java.util.ArrayList;
+
+import task.Task;
+
+public class TaskList {
+    private final ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void add(Task t) {
+        tasks.add(t);
+    }
+
+    public Task get(int i) {
+        return tasks.get(i);
+    }
+
+    public Task remove(int i) {
+        return tasks.remove(i);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public ArrayList<Task> getAll() {
+        return tasks;
+    }
+}
